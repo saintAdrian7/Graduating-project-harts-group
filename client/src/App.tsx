764d934c52284689e4a4a4f10177ab50efe46772
necros240 <iannjau6@gmail.com>
2024-07-09 16:14:26 +0300

    First Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import HomePage from "./pages/Homepage/Homepage";
+
+function App() {
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage():JSX.Element{
+    return(
+        <div className="page">
+            Homepage
+        </div>
+    )
+}
