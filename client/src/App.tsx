85adaf0d94a144e58d8cb6218ea83a9007c43d64
BienG3Rd <githubrian331@gmail.com>
2024-07-11 12:33:27 +0300

    react ui changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
-import { LoginRegisterModel } from "./Features/Authentication/Components/LoginRegisterModel/LoginRegisterModel"
-import { Modal } from "./Features/Authentication/Components/Modal"
-import Navbar from "./Features/Components/Navbar"
-import HomePage from "./Pages/Homepage"
+// import { LoginRegisterModel } from "./Features/Authentication/Components/LoginRegisterModel/LoginRegisterModel"
+// import { Modal } from "./Features/Authentication/Components/Modal"
+// import Navbar from "./Features/Components/Navbar"
+// import HomePage from "./Pages/Homepage"
+import { MuiNavbar} from "./Features/Components/MuiNavbar"
+import { MuiPanel} from "./Features/Components/MuiPanel"
 
 function App() {
   return (
     <>
 
-    <Navbar />
+    {/* <Navbar /> */}
+    <MuiNavbar />
+    <MuiPanel />
    
-    <HomePage />
+    {/* <HomePage /> */}
     </>
     
   
---
+++ client/src/Features/Components/MuiNavbar.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Toolbar, Button, ButtonGroup, IconButton } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const MuiNavbar = () => {
+    return(
+       <AppBar>
+            <Toolbar className="top-nav-bar"> 
+                
+            <img src="https://tse4.mm.bing.net/th?id=OIG2.JQKfKP6cxFiMrZwI_m4J&pid=ImgGn" alt="logo" width="40px"/>
+                <h1>EDU-Murphy</h1>
+                <ButtonGroup aria-label="text button group">
+                    <Button variant="text" className="login-button">Sign Up</Button>
+                    <Button variant="text" className="login-button">Login</Button>
+                    <IconButton>
+                        <MenuIcon  className="icon"/>
+                    </IconButton>
+                   
+                </ButtonGroup>
+                
+
+            </Toolbar>
+       </AppBar>
+    )
+}
+
